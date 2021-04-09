20f88a3250071e5665d1e35696dd5016ba29df29
Zvukoper <soundgot@gmail.com>
2021-04-09 11:49:54 +0500

    Basic functions prototyped for client and server: Initiate, Send, Wait.

--- AARecorder App/Source/AARecorder/AARecorder_Client/AARecorderForm.Designer.cs
+++ AARecorder App/Source/AARecorder/AARecorder_Client/AARecorderForm.Designer.cs
@@ -34,25 +34,28 @@
             this.label1 = new System.Windows.Forms.Label();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.MainPanel = new System.Windows.Forms.Panel();
+            this.button19 = new System.Windows.Forms.Button();
+            this.button18 = new System.Windows.Forms.Button();
+            this.button17 = new System.Windows.Forms.Button();
+            this.button16 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
-            this.button7 = new System.Windows.Forms.Button();
-            this.button8 = new System.Windows.Forms.Button();
-            this.button9 = new System.Windows.Forms.Button();
-            this.button10 = new System.Windows.Forms.Button();
-            this.button11 = new System.Windows.Forms.Button();
-            this.button12 = new System.Windows.Forms.Button();
-            this.button13 = new System.Windows.Forms.Button();
-            this.button14 = new System.Windows.Forms.Button();
-            this.button15 = new System.Windows.Forms.Button();
-            this.button16 = new System.Windows.Forms.Button();
-            this.button17 = new System.Windows.Forms.Button();
-            this.button18 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.MainPanel.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -102,6 +105,9 @@
             // 
             // MainPanel
             // 
+            this.MainPanel.Controls.Add(this.label4);
+            this.MainPanel.Controls.Add(this.label3);
+            this.MainPanel.Controls.Add(this.button19);
             this.MainPanel.Controls.Add(this.button18);
             this.MainPanel.Controls.Add(this.button17);
             this.MainPanel.Controls.Add(this.button16);
@@ -129,6 +135,174 @@
             this.MainPanel.Size = new System.Drawing.Size(776, 527);
             this.MainPanel.TabIndex = 5;
             // 
+            // button19
+            // 
+            this.button19.Location = new System.Drawing.Point(203, 68);
+            this.button19.Name = "button19";
+            this.button19.Size = new System.Drawing.Size(191, 23);
+            this.button19.TabIndex = 23;
+            this.button19.Text = "Connect to Pipes";
+            this.button19.UseVisualStyleBackColor = true;
+            this.button19.Click += new System.EventHandler(this.button19_Click);
+            // 
+            // button18
+            // 
+            this.button18.Location = new System.Drawing.Point(203, 274);
+            this.button18.Name = "button18";
+            this.button18.Size = new System.Drawing.Size(191, 23);
+            this.button18.TabIndex = 22;
+            this.button18.Text = "Send EOF@aDat";
+            this.button18.UseVisualStyleBackColor = true;
+            // 
+            // button17
+            // 
+            this.button17.Location = new System.Drawing.Point(6, 274);
+            this.button17.Name = "button17";
+            this.button17.Size = new System.Drawing.Size(191, 23);
+            this.button17.TabIndex = 21;
+            this.button17.Text = "Wait for EOF@aDat";
+            this.button17.UseVisualStyleBackColor = true;
+            // 
+            // button16
+            // 
+            this.button16.Location = new System.Drawing.Point(203, 332);
+            this.button16.Name = "button16";
+            this.button16.Size = new System.Drawing.Size(191, 23);
+            this.button16.TabIndex = 20;
+            this.button16.Text = "Wait for AAR READY@dCom";
+            this.button16.UseVisualStyleBackColor = true;
+            // 
+            // button15
+            // 
+            this.button15.Location = new System.Drawing.Point(203, 303);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(191, 23);
+            this.button15.TabIndex = 19;
+            this.button15.Text = "Read Filename@dCom";
+            this.button15.UseVisualStyleBackColor = true;
+            // 
+            // button14
+            // 
+            this.button14.Location = new System.Drawing.Point(6, 303);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(191, 23);
+            this.button14.TabIndex = 18;
+            this.button14.Text = "Send Filename@dCom";
+            this.button14.UseVisualStyleBackColor = true;
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(203, 245);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(191, 23);
+            this.button13.TabIndex = 17;
+            this.button13.Text = "Send Data@aDat";
+            this.button13.UseVisualStyleBackColor = true;
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(203, 97);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(191, 23);
+            this.button12.TabIndex = 16;
+            this.button12.Text = "Send ARMA READY@aCom";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(6, 245);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(191, 23);
+            this.button11.TabIndex = 15;
+            this.button11.Text = "Read Data@aDat";
+            this.button11.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(203, 214);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(191, 23);
+            this.button10.TabIndex = 14;
+            this.button10.Text = "Wait for READY FOR DATA@dCom";
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(6, 214);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(191, 23);
+            this.button9.TabIndex = 13;
+            this.button9.Text = "Send READY FOR DATA@dCom";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(6, 184);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(191, 23);
+            this.button8.TabIndex = 12;
+            this.button8.Text = "Received RECORD@aCom";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(203, 155);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(191, 23);
+            this.button7.TabIndex = 11;
+            this.button7.Text = "Send RECORD@aCom";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(6, 155);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(191, 23);
+            this.button6.TabIndex = 10;
+            this.button6.Text = "Wait for Command@aCom";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(203, 126);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(191, 23);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "Wait for AAR READY@ dCom";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(6, 126);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(191, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Send AAR READY@dCom";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(6, 97);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(191, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Wait for ARMA READY@aCom";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(6, 68);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(191, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Launch all Pipes";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // label2
             // 
             this.label2.AutoSize = true;
@@ -147,158 +321,24 @@
             this.button1.Text = "Activate";
             this.button1.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // label3
             // 
-            this.button2.Location = new System.Drawing.Point(6, 42);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(191, 23);
-            this.button2.TabIndex = 6;
-            this.button2.Text = "Launch all Pipes";
-            this.button2.UseVisualStyleBackColor = true;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(53, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 13);
+            this.label3.TabIndex = 24;
+            this.label3.Text = "AARecorder App";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
-            // button3
+            // label4
             // 
-            this.button3.Location = new System.Drawing.Point(6, 71);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(191, 23);
-            this.button3.TabIndex = 7;
-            this.button3.Text = "Wait for ARMA READY@aCom";
-            this.button3.UseVisualStyleBackColor = true;
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(6, 100);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(191, 23);
-            this.button4.TabIndex = 8;
-            this.button4.Text = "Send AAR READY@dCom";
-            this.button4.UseVisualStyleBackColor = true;
-            // 
-            // button5
-            // 
-            this.button5.Location = new System.Drawing.Point(203, 100);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(191, 23);
-            this.button5.TabIndex = 9;
-            this.button5.Text = "Wait for AAR READY@ dCom";
-            this.button5.UseVisualStyleBackColor = true;
-            // 
-            // button6
-            // 
-            this.button6.Location = new System.Drawing.Point(6, 129);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(191, 23);
-            this.button6.TabIndex = 10;
-            this.button6.Text = "Wait for Command@aCom";
-            this.button6.UseVisualStyleBackColor = true;
-            // 
-            // button7
-            // 
-            this.button7.Location = new System.Drawing.Point(203, 129);
-            this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(191, 23);
-            this.button7.TabIndex = 11;
-            this.button7.Text = "Send RECORD@aCom";
-            this.button7.UseVisualStyleBackColor = true;
-            // 
-            // button8
-            // 
-            this.button8.Location = new System.Drawing.Point(6, 158);
-            this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(191, 23);
-            this.button8.TabIndex = 12;
-            this.button8.Text = "Received RECORD@aCom";
-            this.button8.UseVisualStyleBackColor = true;
-            // 
-            // button9
-            // 
-            this.button9.Location = new System.Drawing.Point(6, 188);
-            this.button9.Name = "button9";
-            this.button9.Size = new System.Drawing.Size(191, 23);
-            this.button9.TabIndex = 13;
-            this.button9.Text = "Send READY FOR DATA@dCom";
-            this.button9.UseVisualStyleBackColor = true;
-            // 
-            // button10
-            // 
-            this.button10.Location = new System.Drawing.Point(203, 188);
-            this.button10.Name = "button10";
-            this.button10.Size = new System.Drawing.Size(191, 23);
-            this.button10.TabIndex = 14;
-            this.button10.Text = "Wait for READY FOR DATA@dCom";
-            this.button10.UseVisualStyleBackColor = true;
-            // 
-            // button11
-            // 
-            this.button11.Location = new System.Drawing.Point(6, 219);
-            this.button11.Name = "button11";
-            this.button11.Size = new System.Drawing.Size(191, 23);
-            this.button11.TabIndex = 15;
-            this.button11.Text = "Read Data@aDat";
-            this.button11.UseVisualStyleBackColor = true;
-            // 
-            // button12
-            // 
-            this.button12.Location = new System.Drawing.Point(203, 71);
-            this.button12.Name = "button12";
-            this.button12.Size = new System.Drawing.Size(191, 23);
-            this.button12.TabIndex = 16;
-            this.button12.Text = "Send ARMA READY@aCom";
-            this.button12.UseVisualStyleBackColor = true;
-            // 
-            // button13
-            // 
-            this.button13.Location = new System.Drawing.Point(203, 219);
-            this.button13.Name = "button13";
-            this.button13.Size = new System.Drawing.Size(191, 23);
-            this.button13.TabIndex = 17;
-            this.button13.Text = "Send Data@aDat";
-            this.button13.UseVisualStyleBackColor = true;
-            // 
-            // button14
-            // 
-            this.button14.Location = new System.Drawing.Point(6, 277);
-            this.button14.Name = "button14";
-            this.button14.Size = new System.Drawing.Size(191, 23);
-            this.button14.TabIndex = 18;
-            this.button14.Text = "Send Filename@dCom";
-            this.button14.UseVisualStyleBackColor = true;
-            // 
-            // button15
-            // 
-            this.button15.Location = new System.Drawing.Point(203, 277);
-            this.button15.Name = "button15";
-            this.button15.Size = new System.Drawing.Size(191, 23);
-            this.button15.TabIndex = 19;
-            this.button15.Text = "Read Filename@dCom";
-            this.button15.UseVisualStyleBackColor = true;
-            // 
-            // button16
-            // 
-            this.button16.Location = new System.Drawing.Point(203, 306);
-            this.button16.Name = "button16";
-            this.button16.Size = new System.Drawing.Size(191, 23);
-            this.button16.TabIndex = 20;
-            this.button16.Text = "Wait for AAR READY@dCom";
-            this.button16.UseVisualStyleBackColor = true;
-            // 
-            // button17
-            // 
-            this.button17.Location = new System.Drawing.Point(6, 248);
-            this.button17.Name = "button17";
-            this.button17.Size = new System.Drawing.Size(191, 23);
-            this.button17.TabIndex = 21;
-            this.button17.Text = "Wait for EOF@aDat";
-            this.button17.UseVisualStyleBackColor = true;
-            // 
-            // button18
-            // 
-            this.button18.Location = new System.Drawing.Point(203, 248);
-            this.button18.Name = "button18";
-            this.button18.Size = new System.Drawing.Size(191, 23);
-            this.button18.TabIndex = 22;
-            this.button18.Text = "Send EOF@aDat";
-            this.button18.UseVisualStyleBackColor = true;
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(250, 52);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 13);
+            this.label4.TabIndex = 25;
+            this.label4.Text = "Arma + armalib.dll";
             // 
             // AARecorder
             // 
@@ -344,6 +384,9 @@
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button19;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- AARecorder App/Source/AARecorder/AARecorder_Client/AARecorderForm.cs
+++ AARecorder App/Source/AARecorder/AARecorder_Client/AARecorderForm.cs
@@ -7,18 +7,19 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace AARecorder_Server
 {
-
     
+
+
     public partial class AARecorder : Form
     {
 
-       // private NamedPipeClientStream armaCom;
-        // private bool pipeConnected = false;
+
 
         public AARecorder()
         {
@@ -36,27 +37,149 @@ delphiData = ["\\.\pipe\delphiData"] call jayarma2lib_fnc_openPipe;
 armaData = ["\\.\pipe\armaData"] call jayarma2lib_fnc_openPipe;
  */
 
-            NamedPipeServerStream delphiCom = new NamedPipeServerStream("delphiCom", PipeDirection.InOut, NamedPipeServerStream.MaxAllowedServerInstances, PipeTransmissionMode.Message);
-            NamedPipeServerStream armaCom = new NamedPipeServerStream("armaCom", PipeDirection.InOut, NamedPipeServerStream.MaxAllowedServerInstances, PipeTransmissionMode.Message);
+            
             //delphiCom.Connect();
-            var result = await Task.Run(() =>
+
+//            byte[] messageBytes = Encoding.UTF8.GetBytes(response);
+  //          delphiCom.Write(messageBytes, 0, messageBytes.Length);
+    //        ConsoleOutput.Items.Add(response);
+
+
+        }
+
+        NamedPipeServerStream armaDataS;
+        NamedPipeServerStream delphiDataS;
+        NamedPipeServerStream delphiComS;
+        NamedPipeServerStream armaComS;
+
+        NamedPipeClientStream armaDataC;
+        NamedPipeClientStream delphiDataC;
+        NamedPipeClientStream delphiComC;
+        NamedPipeClientStream armaComC;
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            armaDataS = new NamedPipeServerStream("armaData", PipeDirection.InOut, 1, PipeTransmissionMode.Message);
+            delphiDataS = new NamedPipeServerStream("delphiData", PipeDirection.InOut, 1, PipeTransmissionMode.Message);
+            delphiComS = new NamedPipeServerStream("delphiCom", PipeDirection.InOut, 1, PipeTransmissionMode.Message);
+            armaComS = new NamedPipeServerStream("armaCom", PipeDirection.InOut, 1, PipeTransmissionMode.Message);
+            button2.BackColor = Color.Orange;
+            new Thread(() =>
             {
-                delphiCom.WaitForConnection();
-
-                return "Client connected";
-            });
-            ConsoleOutput.Items.Add(result);
-            var response = "AAR READY";
-            byte[] messageBytes = Encoding.UTF8.GetBytes(response);
-            delphiCom.Write(messageBytes, 0, messageBytes.Length);
-            ConsoleOutput.Items.Add(response);
+                Thread.CurrentThread.IsBackground = true;
+                armaDataS.WaitForConnection();
+                delphiDataS.WaitForConnection();
+                delphiComS.WaitForConnection();
+                armaComS.WaitForConnection();
+                button2.BackColor = Color.Lime;
+            }).Start();
 
 
         }
 
-        
+        private void button19_Click(object sender, EventArgs e)
+        {
+            armaDataC = new NamedPipeClientStream("armaData");
+            delphiDataC = new NamedPipeClientStream("delphiData");
+            delphiComC = new NamedPipeClientStream("delphiCom");
+            armaComC = new NamedPipeClientStream("armaCom");
+            button19.BackColor = Color.Orange;
+            new Thread(() =>
+            {
+            armaDataC.Connect();
+                armaDataC.ReadMode = PipeTransmissionMode.Message;
+            delphiDataC.Connect();
+                delphiDataC.ReadMode = PipeTransmissionMode.Message;
+            delphiComC.Connect();
+                delphiComC.ReadMode = PipeTransmissionMode.Message;
+            armaComC.Connect();
+                armaComC.ReadMode = PipeTransmissionMode.Message;
+                button19.BackColor = Color.Lime;
+            }).Start();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            StringBuilder messageBuilder = new StringBuilder();
+            var response = "";
+            string messageChunk = string.Empty;
+            byte[] messageBuffer = new byte[5];
+            button3.BackColor = Color.Orange;
+            var loop = true;
+            new Thread(() =>
+            {
+                do
+            {
+                armaComS.Read(messageBuffer, 0, messageBuffer.Length);
+                messageChunk = Encoding.UTF8.GetString(messageBuffer);
+                messageBuilder.Append(messageChunk);
+                messageBuffer = new byte[messageBuffer.Length];
+                response = messageBuilder.ToString();
+                if (response == "ARMA READY") { button3.BackColor = Color.Lime; loop = false; }
+            }
+            while (loop == true);
+            }).Start();
+            
+
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            button12.BackColor = Color.Orange;
+            new Thread(() =>
+            {
+            byte[] messageBytes = Encoding.UTF8.GetBytes("ARMA READY");
+            armaComC.Write(messageBytes, 0, messageBytes.Length);
+            }).Start();
+            button12.BackColor = Color.Lime;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            new Thread(() =>
+            {
+                button4.BackColor = Color.Orange;
+            //delphiComS.WaitForConnection();
+                byte[] messageBytes = Encoding.UTF8.GetBytes("AAR READY");
+                delphiComS.Write(messageBytes, 0, messageBytes.Length);
+            ConsoleOutput.Items.Add(string.Format("Sending AAR READY to delphiComS {0}", delphiComC.IsConnected));
+            button4.BackColor = Color.Lime;
+            }).Start();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            StringBuilder messageBuilder = new StringBuilder();
+            var response = "";
+            string messageChunk = string.Empty;
+            byte[] messageBuffer = new byte[5];
+            button5.BackColor = Color.Orange;
+            
+            new Thread(() =>
+            {
+                do
+                {
+                    //delphiComC.Connect();
+                    delphiComC.WaitForPipeDrain();
+                    delphiComC.Read(messageBuffer, 0, messageBuffer.Length);
+                    messageChunk = Encoding.UTF8.GetString(messageBuffer);
+                    messageBuilder.Append(messageChunk);
+                    messageBuffer = new byte[messageBuffer.Length];
+                    response = messageBuilder.ToString();
+                    
+                }
+                while ( (!delphiComC.IsMessageComplete) && (response == "AAR READY"));
+                button5.BackColor = Color.Lime;
+                button4.BackColor = Color.Lime;
+            }).Start();
 
 
         }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
     
 }
